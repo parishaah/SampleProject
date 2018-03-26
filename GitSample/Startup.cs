e17f52b211df23f3028e1909a891047c72c1cade
najafi <najafi@ESAMDEV9>
2018-03-26 11:53:00 +0430

    Add project files.

---
+++ GitSample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitSample.Startup))]
+namespace GitSample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
